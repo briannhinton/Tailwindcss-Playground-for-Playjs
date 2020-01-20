d31b2d78a57f8a30c3749614ea90927a2a925915
Brian Hinton <briannhinton@gmail.com>
2020-01-19 22:20:06 -0500

    Create webpack.config.dev.js

---
+++ webpack.config.dev.js
@@ -0,0 +1,30 @@
+const port = process.env['WEB_APP_PORT'];
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/styles.css',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            'postcss-loader',
+          ],
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css', {
+      disable: process.env.NODE_ENV === 'development',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+}
