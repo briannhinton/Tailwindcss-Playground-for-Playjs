d6ec9c7e9944f5025bd16df04a16c1548f2414fe
Brian Hinton <mrbrianhinton@users.noreply.github.com>
2020-02-01 00:53:35 -0500

    Working build now

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, './public'),
     publicPath: '/'
   },
